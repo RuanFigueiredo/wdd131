cf493766a9fea53666f6b42bca9b70961550bdbf
RuanFigueiredo <ruanthiago67@gmail.com>
2026-09-10 19:47:05 -0300

    estilo hover e javascript alterado

--- scripts/obterdatas.js
+++ scripts/obterdatas.js
@@ -1,9 +1,14 @@
-const dataAtual = new Date();
-const anoAtual = dataAtual.getFullYear();
-
-document.getElementById("anoAtual").textContent = anoAtual;
-
-const dataModificacao =  new Date(document.lastModified);
-
-
-document.getElementById("ultimaModificacao").textContent = dataModificacao.toLocaleDateString('pt-BR', { day: '2-digit', month: '2-digit', year: 'numeric' });
+//obter ano atual
+document.addEventListener("DOMContentLoaded", () => {
+const anoAtual = new Date().getFullYear();
+const elementoDoAno = document.querySelector("#anoAtual");
+if(elementoDoAno) {
+    elementoDoAno.textContent = anoAtual;
+}
+//obter  ultima data de modificação
+const dataModificacao = new Date(document.lastModified);
+const elementoDaModificacao = document.querySelector("#ultimaModificacao");
+if(elementoDaModificacao) {
+    elementoDaModificacao.textContent = dataModificacao.toLocaleDateString('pt-BR', { day: '2-digit', month: '2-digit', year: 'numeric' });
+}
+});
