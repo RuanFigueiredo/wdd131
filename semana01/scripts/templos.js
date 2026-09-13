518dafa977b92182325a52545ef2035e3bc4ce67
RuanFigueiredo <ruanthiago67@gmail.com>
2026-09-13 01:03:38 -0300

    página de imagens realizada

---
+++ semana01/scripts/templos.js
@@ -0,0 +1,15 @@
+//obter ano atual
+document.addEventListener("DOMContentLoaded", () => {
+const anoAtual = new Date().getFullYear();
+const elementoDoAno = document.querySelector("#anoAtual");
+if(elementoDoAno) {
+    elementoDoAno.textContent = anoAtual;
+}
+//obter  ultima data de modificação
+const dataModificacao = new Date(document.lastModified);
+const elementoDaModificacao = document.querySelector("#ultimaModificacao");
+if(elementoDaModificacao) {
+    elementoDaModificacao.textContent = dataModificacao.toLocaleDateString('pt-BR', { day: '2-digit', month: '2-digit', year: 'numeric' });
+}
+});
+
